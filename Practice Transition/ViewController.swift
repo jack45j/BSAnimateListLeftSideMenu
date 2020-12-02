917f93c91befe01bad9c8505cfad9b3012f41bc1
Yi-Cheng Lin <jack45j@gmail.com>
2020-12-02 21:26:48 +0800

    Initial Commit

---
+++ Practice Transition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice Transition
+//
+//  Created by Yi-Cheng Lin on 2020/12/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
